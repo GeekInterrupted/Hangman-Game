a371f443b4e16beeb098a35779d716365c65c722
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-03-26 21:30:34 -0500

    Hangman game assignment - starter files

---
+++ assets/js/hangman.js
@@ -0,0 +1,127 @@
+document.getElementById("counter-wins").innerHTML = "Wins: 0";
+document.getElementById("counter-losses").innerHTML = "Losses: 0";
+document.getElementById("guesses-left").innerHTML = "Guesses left: 15";
+document.getElementById("answer").innerHTML = " _ ";
+document.getElementById("user-guesses").innerHTML = "You typed ";
+document.getElementById("computer-choice").innerHTML = "The answer is: ";
+            
+        
+var VILLAINS = [
+    "weeping angels",
+    "daleks",
+    "cybermen",
+    "the silence",
+    "silurians",
+    "ice warriors",
+    "davros",
+    "the master",
+    "zygons",
+    "ood",
+    "vashta nerada",
+    "autons",
+    "the rani",    
+];
+
+//variables to store words being guessed
+
+var currentWordGuessed="";
+var answerArray = [];
+var userArray = [];
+var gameOver = 0;
+var winCount = 0;
+var lossesCount = 0;
+var guessesLeft = 15;
+var usedLetters = [];
+            
+
+//game control
+            
+            
+            
+
+
+function init() {
+    currentWordGuessed=VILLAINS[Math.floor(Math.random() * VILLAINS.length)];
+    answerArray = [];
+    for (var i = 0; i < currentWordGuessed.length; i++) {
+        answerArray[i] = " _ ";
+        console.log(currentWordGuessed);
+       
+       
+ }
+    console.log(answerArray.join(""));
+    document.getElementById("answer").innerHTML = answerArray.join("");
+    
+
+
+}
+init();
+
+//get user input
+function userGuess() {
+       
+        var losses = 0;
+        var wins = 0;
+        document.onkeyup = function(event) {
+// Determine which key was pressed
+        var userGuess = event.key;
+        userGuessLowerCase = String.fromCharCode(event.keyCode).toLowerCase();
+        console.log(userGuessLowerCase);
+        
+
+//update the user guess
+        for (var i = 0; i < currentWordGuessed.length; i++) {
+            if (userGuess === currentWordGuessed[i]) {
+                answerArray[i] = userGuess;
+                console.log(userGuess);
+                document.getElementById("answer").innerHTML = answerArray.join("");
+                
+        
+            }
+         }
+            if (answerArray.indexOf(" _ ") === -1) {
+                var cumulativeUserGuesses =usedLetters.push(userGuess);
+                document.getElementById("user-guesses").innerHTML = ("You typed: " + usedLetters);
+                init();
+                wins ++;
+                guessesLeft = 10;
+                restart();
+                console.log(wins);
+                document.getElementById("counter-wins").innerHTML = ("Wins: " + wins);
+                             
+            } 
+            if (answerArray.indexOf(" _ ") !== -1) {
+                guessesLeft --;
+                var cumulativeUserGuesses =usedLetters.push(userGuess);
+                document.getElementById("user-guesses").innerHTML = ("You typed: " + usedLetters);
+                document.getElementById("guesses-left").innerHTML = ("Guesses left: " + guessesLeft);
+               
+                
+                 
+            }
+            if (guessesLeft === 0) {
+                document.getElementById("computer-choice").innerHTML = "The answer is: " + currentWordGuessed;
+                init();
+                losses++;
+                guessesLeft = 15;
+                document.getElementById("counter-losses").innerHTML = ("Losses: " + losses);
+                restart();
+                
+                
+            }
+       }
+}
+
+userGuess();
+            
+            
+function restart() {
+    while(usedLetters.length > 0) {
+    usedLetters.pop();
+}
+       
+    document.getElementById("user-guesses").innerHTML = "You typed: ";
+   
+        
+}       
+         
